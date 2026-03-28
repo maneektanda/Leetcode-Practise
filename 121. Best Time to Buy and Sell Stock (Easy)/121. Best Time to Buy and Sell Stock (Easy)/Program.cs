1edf70d4e0fa66e73824b1f05cc1c53e1902fe44
Maneek Tanda <mick_511@hotmail.com>
2026-03-29 09:31:19 +1000

    Adding Q121 and updating some folder names to include difficulty level.

---
+++ 121. Best Time to Buy and Sell Stock (Easy)/121. Best Time to Buy and Sell Stock (Easy)/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _121._Best_Time_to;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    int MaxProfit(int[] prices) {
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.Length - 1; i++){
+            int buyPrice = prices[i];
+            for (int j = i + 1; j < prices.Length; j++){
+                int sellPrice = prices[j];
+                int profit = sellPrice - buyPrice;
+                if (profit > maxProfit){
+                    maxProfit = profit;
+                }
+            }
+        }
+        return maxProfit;
+    }
+}
