657a8668e9e7576488ca208008f2bbfac656ed21
Maneek Tanda <mick_511@hotmail.com>
2026-05-08 22:56:04 +1000

    Q153

---
+++ 153. Find Minimum in Rotated Sorted Array/Solution/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace Solution;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    public int FindMin(int[] nums)
+    {
+        int l = 0; 
+        int r = nums.Length - 1;
+        int min = nums[0];
+
+        while (l <= r)
+        {
+            if (nums[l] < nums[r])
+            {
+                return Math.Min(min, nums[l]);
+            }
+            int m = r - ((r - l) / 2);
+            min = Math.Min(min, nums[m]);
+            if (nums[m] >= nums[l])
+            {
+                l = m + 1;
+            }
+            else
+            {
+                r = m - 1;
+            }
+        }
+        return min;
+    }
+}
