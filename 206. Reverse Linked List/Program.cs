4311c8201da9d30da9ae0ac7856f65c88702f83b
Maneek Tanda <mick_511@hotmail.com>
2026-05-09 12:44:57 +1000

    Q206

---
+++ 206. Reverse Linked List/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace _206._Reverse_Linked_List;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+    public ListNode ReverseList(ListNode head)
+    {
+        ListNode curr = head;
+        ListNode prev = null;
+
+        while (curr != null)
+        {
+            ListNode temp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = temp;
+        }
+        return prev;
+    }
+}
+
+
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
