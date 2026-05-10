02202b208ea605e1d3ccddec493a2009c7025085
Maneek Tanda <mick_511@hotmail.com>
2026-05-10 21:51:40 +1000

    Q141

---
+++ 141. Linked List Cycle/Solution/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Solution;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    public bool HasCycle(ListNode head) {
+        HashSet<ListNode> seen = new HashSet<ListNode>();
+
+        while(head != null){
+            if (seen.Contains(head)){
+                return true;
+            }
+            seen.Add(head);
+            head = head.next;
+        }
+        return false;
+    }
+
+  //Deinition for singly-linked list.
+    public class ListNode {
+        public int val;
+        public ListNode next;
+        public ListNode(int val=0, ListNode next=null) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+ 
+}
