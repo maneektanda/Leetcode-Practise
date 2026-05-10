0b7329ed459a43eedc5f603149cb3d9d113ed612
Maneek Tanda <mick_511@hotmail.com>
2026-05-10 23:11:51 +1000

    Q143

---
+++ 143. Reorder Linked List/Solution/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace Solution;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+
+    public void ReorderList(ListNode head)
+    {
+        List<ListNode> nodes = new List<ListNode>();
+        while (head != null)
+        {
+            nodes.Add(head);
+            head = head.next;
+        }
+
+        if (nodes.Count == 1) return;
+
+        for (int i = 0; i < nodes.Count / 2 ; i++)
+        {
+            nodes[i].next = nodes[^(i + 1)];
+            nodes[^(i + 1)].next = nodes[i + 1];
+        }
+
+        if (nodes.Count % 2 != 0)
+        {
+            nodes[(nodes.Count / 2) + 1].next = nodes[nodes.Count / 2];
+        }            
+
+        nodes[nodes.Count / 2].next = null;
+    }
+
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int val = 0, ListNode next = null)
+        {
+            this.val = val;
+            this.next = next;
+        }
+    }
+}
